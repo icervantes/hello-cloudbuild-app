c64b3c3c7d0fb156c12db816fa8873fdd5ebbfd6
icervantes <lord.icervantes@gmail.com>
2024-10-22 23:17:16 -0500

    Update message to be rollback.

--- test_app.py
+++ test_app.py
@@ -18,7 +18,7 @@ from app import hello
 class TestHelloApp(unittest.TestCase):
 
   def test_hello(self):
-    self.assertEqual(hello(), "Hello World Spike!\n")
+    self.assertEqual(hello(), "Hello World Ingestion team!\n")
 
 if __name__ == '__main__':
   unittest.main()
